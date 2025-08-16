979ee22a54d718c1fc315517f7cc817e9a96dd15
Ryan Cole <admin@ryanc.me>
2025-08-16 18:07:56 +1200

    Set users.mutableUsers = false

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -12,16 +12,19 @@
     };
   };
 
-  users.users = {
-    ryan = {
-      isNormalUser = true;
-      hashedPasswordFile = config.sops.secrets."ryan/password".path;
-      extraGroups = [ "wheel" ];
-    };
-    angel = {
-      isNormalUser = true;
-      hashedPasswordFile = config.sops.secrets."angel/password".path;
-      extraGroups = [ "wheel" ];
+  users = {
+    mutableUsers = false;
+    users = {
+      ryan = {
+        isNormalUser = true;
+        hashedPasswordFile = config.sops.secrets."ryan/password".path;
+        extraGroups = [ "wheel" ];
+      };
+      angel = {
+        isNormalUser = true;
+        hashedPasswordFile = config.sops.secrets."angel/password".path;
+        extraGroups = [ "wheel" ];
+      };
     };
   };
 }
