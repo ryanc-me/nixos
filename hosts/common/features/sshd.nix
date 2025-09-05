87313113ba85d1abdf7a55ce07aeb8d68f45b457
Ryan Cole <admin@ryanc.me>
2025-09-06 09:33:59 +1200

    Add sshd

---
+++ hosts/common/features/sshd.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
+}
