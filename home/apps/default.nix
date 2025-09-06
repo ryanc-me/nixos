d8ac58e8ce60d48dbdf88353cd4dbead64bc467d
Ryan Cole <admin@ryanc.me>
2025-09-06 12:09:03 +1200

    Add toggl track

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -38,5 +38,20 @@
         StartupWMClass = "msedge-outlook.office.com__mail_-Default";
       };
     };
+
+    # toggl track
+    dataFile."icons/hicolor/scalable/apps/toggl-track.svg".source = ./icons/toggl-track.svg;
+    desktopEntries.toggl-track = lib.mkForce {
+      name = "Toggl Track";
+      comment = "Launch Toggl Track";
+      categories = [ "Utility" ];
+      icon = "toggl-track";
+      exec = ''
+        ${pkgs.microsoft-edge}/bin/microsoft-edge --enable-features=UseOzonePlatform,WebUIDarkMode --ozone-platform-hint=wayland --force-dark-mode --app="https://track.toggl.com/timer" %U
+      '';
+      settings = {
+        StartupWMClass = "msedge-track.toggl.com__timer_-Default";
+      };
+    };
   };
 }
