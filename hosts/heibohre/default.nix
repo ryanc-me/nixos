7e52ab770c9df6662a96a7cfc66210ed05d78f29
Ryan Cole <admin@ryanc.me>
2025-09-13 19:18:13 +1200

    Refactor heibohre/default.nix
    Mainly just tidying things up

--- hosts/heibohre/default.nix
+++ hosts/heibohre/default.nix
@@ -8,7 +8,6 @@ in
     ./hardware-configuration.nix
     ../../modules/nixos
   ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # device-specific
   my = {
@@ -22,61 +21,57 @@ in
     };
   };
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Data disk & swap are encrypted with LUKS
-  boot.initrd.luks.devices = {
-    root = {
-      device = "/dev/nvme0n1p2";
-      preLVM = true;
-    };
-  };
-
-  # required to get the keyboard working during boot for LUKS decryption
-  # note that the order *is* important!
-  boot.initrd.availableKernelModules = [
-    "pinctrl_icelake"
-    "surface_aggregator"
-    "surface_aggregator_registry"
-    "surface_aggregator_hub"
-    "surface_hid_core"
-    "8250_dw"
-    "surface_hid"
-    "intel_lpss"
-    "intel_lpss_pci"
-  ];
-
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  networking.hostName = "heibohre";
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  hardware.bluetooth.enable = true;
   time.timeZone = "Pacific/Auckland";
 
+  #TODO: move this somewhere else?
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
   ];
-
-  programs.dconf.profiles.gdm.databases = [{
-    settings."org/gnome/mutter" = {
-      experimental-features = ["scale-monitor-framebuffer"];
-    };
-    settings."org/gnome/desktop/interface" = {
-      cursor-theme = "Capitaine Cursors";
-      cursor-size = lib.gvariant.mkInt32 24;
-    };
-  }];
 
   # for GDM
   systemd.tmpfiles.rules = [
     "L+ /run/gdm/.config/monitors.xml - gdm gdm - ${monitors-xml}"
   ];
 
-  hardware.bluetooth.enable = true;
+  networking = {
+    hostName = "heibohre";
+    networkmanager.enable = true;
+  };
+
+  boot = {
+    loader = {
+      # Use the systemd-boot EFI boot loader.
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    initrd = {
+      luks.devices = {
+        root = {
+          device = "/dev/nvme0n1p2";
+          preLVM = true;
+        };
+      };
+
+      # required to get the keyboard working during boot for LUKS decryption
+      # note that the order *is* important!
+      availableKernelModules = [
+        "pinctrl_icelake"
+        "surface_aggregator"
+        "surface_aggregator_registry"
+        "surface_aggregator_hub"
+        "surface_hid_core"
+        "8250_dw"
+        "surface_hid"
+        "intel_lpss"
+        "intel_lpss_pci"
+      ];
+    };
+
+    # Use latest kernel.
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 
   system.stateVersion = "25.05";
 }
