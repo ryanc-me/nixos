abd91a3ab3e4e85514d2ac551ccfe4ca9bf80aeb
Ryan Cole <admin@ryanc.me>
2025-09-19 15:54:31 +1200

    Set firefox as default browser

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -35,4 +35,13 @@
 
   # fonts
   fonts.packages = with pkgs; [];
+
+  # default apps
+  xdg.mime.defaultApplications = {
+    "text/html" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+    "x-scheme-handler/about" = "firefox.desktop";
+    "x-scheme-handler/unknown" = "firefox.desktop";
+  };
 }
