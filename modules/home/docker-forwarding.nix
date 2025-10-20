d03f876a10eebdf56385f9eeb8a1597e2193f8ef
Ryan Cole <admin@ryanc.me>
2025-10-20 15:42:25 +1300

    Fix-up docker forwarding

--- modules/home/docker-forwarding.nix
+++ modules/home/docker-forwarding.nix
@@ -2,30 +2,18 @@
 
 {
   systemd.user.services = {
-    "docker-socket-tunnel@" = {
-      Unit = {
-        Description = "Docker Socket Tunnel for %i";
-        After = [ "network.target" ];
-      };
-      Service = {
-        WorkingDirectory = config.home.homeDirectory;
-        ExecStartPre = [ "${pkgs.coreutils}/bin/rm -f ${config.home.homeDirectory}/.docker.%i.sock" ];
-        ExecStart = "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -L ${config.home.homeDirectory}/.docker.%i.sock:/var/run/docker.sock %i";
-        RestartSec = 5;
-        Restart = "always";
-      };
-    };
-
     # for now, we'll define these as concrete services
-    "docker-socket-tunnel@wedoo-demo.service" = {
+    "docker-socket-tunnel--wedoo-demo" = {
       Unit = {
         Description = "Docker Socket Tunnel for %i";
         After = [ "network.target" ];
       };
       Service = {
         WorkingDirectory = config.home.homeDirectory;
-        ExecStartPre = [ "${pkgs.coreutils}/bin/rm -f ${config.home.homeDirectory}/.docker.%i.sock" ];
-        ExecStart = "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -L ${config.home.homeDirectory}/.docker.%i.sock:/var/run/docker.sock %i";
+        ExecStartPre = [
+          "${pkgs.coreutils}/bin/rm -f ${config.home.homeDirectory}/.docker.wedoo-demo.sock"
+        ];
+        ExecStart = "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -L ${config.home.homeDirectory}/.docker.wedoo-demo.sock:/var/run/docker.sock wedoo-demo";
         RestartSec = 5;
         Restart = "always";
       };
@@ -33,15 +21,17 @@
         WantedBy = [ "graphical-session.target" ];
       };
     };
-    "docker-socket-tunnel@wedoo-docker1.service" = {
+    "docker-socket-tunnel--wedoo-docker1" = {
       Unit = {
-        Description = "Docker Socket Tunnel for %i";
+        Description = "Docker Socket Tunnel for wedoo-docker1";
         After = [ "network.target" ];
       };
       Service = {
         WorkingDirectory = config.home.homeDirectory;
-        ExecStartPre = [ "${pkgs.coreutils}/bin/rm -f ${config.home.homeDirectory}/.docker.%i.sock" ];
-        ExecStart = "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -L ${config.home.homeDirectory}/.docker.%i.sock:/var/run/docker.sock %i";
+        ExecStartPre = [
+          "${pkgs.coreutils}/bin/rm -f ${config.home.homeDirectory}/.docker.wedoo-docker1.sock"
+        ];
+        ExecStart = "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -L ${config.home.homeDirectory}/.docker.wedoo-docker1.sock:/var/run/docker.sock wedoo-docker1";
         RestartSec = 5;
         Restart = "always";
       };
@@ -51,7 +41,25 @@
     };
   };
 
-  #TODO: auto-enable these?
+  #TODO: get this working
+  #
+  # the service itself is okay, but I can't figure out how to enable the service
+  # via nix
+
+  # "docker-socket-tunnel@" = {
+  #   Unit = {
+  #     Description = "Docker Socket Tunnel for %i";
+  #     After = [ "network.target" ];
+  #   };
+  #   Service = {
+  #     WorkingDirectory = config.home.homeDirectory;
+  #     ExecStartPre = [ "${pkgs.coreutils}/bin/rm -f ${config.home.homeDirectory}/.docker.%i.sock" ];
+  #     ExecStart = "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -L ${config.home.homeDirectory}/.docker.%i.sock:/var/run/docker.sock %i";
+  #     RestartSec = 5;
+  #     Restart = "always";
+  #   };
+  # };
+
   # systemctl --user enable --now docker-socket-tunnel@wedoo-demo.service
   # systemctl --user enable --now docker-socket-tunnel@wedoo-docker1.service
 }
