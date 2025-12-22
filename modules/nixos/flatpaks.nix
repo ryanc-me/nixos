df79cdba6fe04f95dd6720f3af1674c810853a4f
Ryan Cole <admin@ryanc.me>
2025-12-22 17:16:17 +1300

    Add explicit XDG portals (for Flatpaks)

--- modules/nixos/flatpaks.nix
+++ modules/nixos/flatpaks.nix
@@ -16,4 +16,10 @@
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk      
+    ];
+  };
 }
