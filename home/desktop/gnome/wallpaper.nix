032825fc495f574f939c53f7485d98916e204877
Ryan Cole <admin@ryanc.me>
2026-01-04 12:34:50 +1300

    Gracefully handle unconfigured options (part 2)
    Continuation of #4ffaa0dd

--- home/desktop/gnome/wallpaper.nix
+++ home/desktop/gnome/wallpaper.nix
@@ -5,9 +5,11 @@
   ...
 }:
 let
-  gnome = osConfig.mine.desktop-gnome.gnome;
-  wallpaper = osConfig.mine.desktop.system.wallpaper;
-  wp = wallpaper.processed.plain."${wallpaper.mode}";
+  hasDesktop = osConfig.mine ? desktop-gnome && osConfig.mine.desktop-gnome ? gnome;
+
+  gnome = if hasDesktop then osConfig.mine.desktop-gnome.gnome else { enable = false; };
+  wallpaper = if hasDesktop then osConfig.mine.desktop.system.wallpaper else { enable = false; };
+  wp = if hasDesktop then wallpaper.processed.plain."${wallpaper.mode}" else "";
 in
 {
   config = lib.mkIf (wallpaper.enable && gnome.enable) {
