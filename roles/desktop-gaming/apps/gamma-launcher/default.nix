02c39ab3165fef0dda67cc8478e80f8a1f0c05c4
Ryan Cole <admin@ryanc.me>
2026-01-11 13:42:10 +1300

    Add some refs for gamma-launcher

--- roles/desktop-gaming/apps/gamma-launcher/default.nix
+++ roles/desktop-gaming/apps/gamma-launcher/default.nix
@@ -14,6 +14,10 @@ in
     enable = mkEnableOption "gamma-launcher";
   };
 
+  # https://github.com/Red007Master/Red-s-Guide-on-Installing-G.A.M.M.A.-on-Linux
+  # https://github.com/Mord3rca/gamma-launcher
+  # https://github.com/DravenusRex/stalker-gamma-linux-guide
+  # https://gist.github.com/v1ld/e9069af307bd90495e0b345f3a260725
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       gamma-launcher
