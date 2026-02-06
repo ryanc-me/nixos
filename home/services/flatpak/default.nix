9593242d06129c5cf6ece4eb9fc984fb572b0125
Ryan Cole <admin@ryanc.me>
2026-02-06 20:32:50 +1300

    Only enable flatpaks if also enabled on NixOS side

--- home/services/flatpak/default.nix
+++ home/services/flatpak/default.nix
@@ -2,19 +2,21 @@
   config,
   pkgs,
   lib,
+  osConfig,
   ...
 }:
 
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.mine.home.services.flatpak;
+  nixosCfg = osConfig.mine.desktop.services.flatpak;
 in
 {
   options.mine.home.services.flatpak = {
     enable = mkEnableOption "flatpak support";
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.enable && nixosCfg.enable) {
     services.flatpak = {
       enable = true;
       remotes = {
