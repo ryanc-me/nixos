74cd9b283d260917ac087f2ca9c3e07a150e8276
Ryan Cole <admin@ryanc.me>
2026-02-07 23:42:56 +1300

    Use auth.mixeto.io for redirect
    GitHub started rejecting auth requests, citing "invalid redirect_uri". I'm 90% sure nothing changed here, so perhaps it was a change on their end?
    
    Either way, this fixed-return method works nicely

--- roles/server-nginx/domains/mixeto.io/default.nix
+++ roles/server-nginx/domains/mixeto.io/default.nix
@@ -15,7 +15,7 @@ in
   options.mine.server-nginx.domains.mixeto-io = {
     enable = mkEnableOption "mixeto.io domain";
   };
-  
+
   config = mkIf cfg.enable {
     security.acme = mkIf certs.enable {
       acceptTerms = true;
@@ -39,13 +39,34 @@ in
         forceSSL = true;
         useACMEHost = "mixeto.io";
         acmeRoot = null;
-        locations."/".return = "404";
+        locations."/".return = "307 https://www.mixeto.io";
       };
       "www.mixeto.io" = {
         forceSSL = true;
         useACMEHost = "mixeto.io";
         acmeRoot = null;
-        locations."/".return = "404";
+        locations."/".return = "307 https://www.ryanc.me";
+      };
+      "auth.mixeto.io" = {
+        forceSSL = true;
+        useACMEHost = "mixeto.io";
+        acmeRoot = null;
+        locations."= /".return = "307 https://www.mixeto.io";
+
+        locations."/oauth2/" = {
+          proxyPass = "http://127.0.0.1:4180";
+        };
+        locations."~ \"^/redirect/(?<r_host>(?:[A-Za-z0-9-]+\\.)*(?:ryanc\\.me|mixeto\\.io))(?<r_path>/[A-Za-z0-9\\-._~!$&'()*+,;=:@/%]*)?$\"" =
+          {
+            extraConfig = ''
+              if ($request_uri ~* "(%0d|%0a|\\r|\\n)") { return 400; }
+              if ($args       ~* "(%0d|%0a|\\r|\\n)") { return 400; }
+              set $safe_path $r_path;
+              if ($safe_path = "") { set $safe_path "/"; }
+
+              return 307 https://$r_host$safe_path$is_args$args;
+            '';
+          };
       };
     };
   };
