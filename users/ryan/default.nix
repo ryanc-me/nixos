07b55a0f8d729314fab5c10dd0fa9619e84c4615
Ryan Cole <admin@ryanc.me>
2026-03-13 18:09:08 +1300

    Add left-monitor script (`Mod+Ctrl+L`)

--- users/ryan/default.nix
+++ users/ryan/default.nix
@@ -61,5 +61,52 @@
     ];
   };
 
+  # TODO: only when desktop-niri is enabled, only for idir
+  home.file.".local/bin/left-monitor.hm-init" = {
+    text = ''
+      #!/usr/bin/env bash
+      set -eu
+
+      niri msg action focus-workspace "side" || true
+
+      microsoft-outlook &
+      microsoft-teams &
+      spotify &
+      subl &
+
+      sleep 1
+
+      niri msg action focus-column-first
+      niri msg action set-column-display tabbed
+
+
+      subl_window_id=$(niri msg --json windows | ${pkgs.jq}/bin/jq -r --arg app "sublime_text" '
+          [.[] | select(.app_id == $app)] | first | .id // empty
+      ')
+
+      echo "subl_window_id = ''${subl_window_id}"
+      app_ids='
+      msedge-outlook.office.com__mail_-Default
+      msedge-teams.microsoft.com__v2_-Default
+      msedge-open.spotify.com__-Default
+      '
+      for app in $app_ids; do
+          window_id=$(niri msg --json windows | ${pkgs.jq}/bin/jq -r --arg app "$app" '
+              [.[] | select(.app_id == $app)] | first | .id // empty
+          ')
+          echo "window_id for ''${app} = ''${window_id}"
+          niri msg action focus-window --id "$window_id"
+          niri msg action move-column-to-index 2
+          niri msg action focus-window --id "$subl_window_id"
+          niri msg action consume-window-into-column || true
+      done
+    '';
+    onChange = ''
+      rm -f .local/bin/left-monitor
+      cp .local/bin/left-monitor.hm-init .local/bin/left-monitor
+      chmod u+x .local/bin/left-monitor
+    '';
+  };
+
   home.stateVersion = "25.05";
 }
