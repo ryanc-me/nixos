51c42333eaa465400c937e1d0f46124ee01486fa
Ryan Cole <admin@ryanc.me>
2026-03-16 08:41:07 +1300

    Tweak Nvidia driver setup

--- roles/desktop/system/gpu-nvidia/default.nix
+++ roles/desktop/system/gpu-nvidia/default.nix
@@ -16,40 +16,60 @@ in
 
   config = mkIf cfg.enable {
     # https://nixos.wiki/wiki/Nvidia
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = [ pkgs.gamescope-wsi ];
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+          gamescope-wsi
+          nvidia-vaapi-driver
+          libvdpau-va-gl
+          libva-vdpau-driver
+        ];
+      };
+      nvidia = {
+        modesetting.enable = true;
+        powerManagement = {
+          enable = true;
+          # finegrained = true;
+        };
+        open = true;
+        nvidiaSettings = true;
+
+        # stable driver
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+        # or, specific version
+        # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        #   version = "580.95.05";
+        #   sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+        #   sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+        #   openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+        #   settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+        #   persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
+        # };
+      };
+      opengl.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      nvtopPackages.nvidia
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-tools
+    ];
+
+    environment.variables = {
+      GBM_BACKEND = "nvidia-drm";
+      LIBVA_DRIVER_NAME = "nvidia";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      __GL_GSYNC_ALLOWED = "1";
+      __GL_VRR_ALLOWED = "1";
     };
 
     services.xserver.videoDrivers = [ "nvidia" ];
 
     powerManagement.enable = true;
 
-    hardware.nvidia = {
-      modesetting.enable = true;
-      powerManagement = {
-        enable = true;
-        # finegrained = true;
-      };
-      open = false;
-      nvidiaSettings = true;
-
-      # stable driver
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-      # or, specific version
-      # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      #   version = "580.95.05";
-      #   sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-      #   sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
-      #   openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-      #   settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-      #   persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
-      # };
-    };
-    environment.systemPackages = with pkgs; [
-      nvtopPackages.nvidia
-    ];
   };
 }
