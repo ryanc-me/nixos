b3e2f2f2d577e63079c02034fe40473dbc02fa03
Ryan Cole <admin@ryanc.me>
2026-03-16 23:24:11 +1300

    Add GPU-related settings for Mixeto

--- hosts/masaq/default.nix
+++ hosts/masaq/default.nix
@@ -54,6 +54,20 @@ in
 
   #TODO: move this to a role
   virtualisation.docker.enable = true;
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        vpl-gpu-rt
+        intel-compute-runtime
+      ];
+    };
+  };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
   environment.systemPackages = with pkgs; [
     # for intel_gpu_top
     intel-gpu-tools
