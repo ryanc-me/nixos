48cd0b5bcdf7331417022e227b95544eea580120
Ryan Cole <admin@ryanc.me>
2026-04-19 11:25:19 +1200

    Add trusted-users for remote build

--- roles/core/system/nix/default.nix
+++ roles/core/system/nix/default.nix
@@ -6,10 +6,16 @@
 }:
 
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
 
   nixpkgs.config = {
     # 1password, sublime4, etc
