aed3bf7b0c293fb010ebc81bf31972e39a192a80
Ryan Cole <admin@ryanc.me>
2026-05-02 20:31:11 +1200

    Setup Authentik BP for Frigate

--- roles/server-home/services/frigate/default.nix
+++ roles/server-home/services/frigate/default.nix
@@ -10,6 +10,44 @@ let
   cfg = config.mine.server-home.services.frigate;
   nginx = config.mine.server-nginx.services.nginx;
   hostname = "frigate.${config.mine.server-nginx.domainBase}";
+
+  frigateBlueprint = pkgs.writeText "authentik-frigate.yaml" ''
+    version: 1
+    metadata:
+      name: frigate
+      labels:
+        blueprints.goauthentik.io/instantiate: "true"
+
+    entries:
+      - model: authentik_providers_proxy.proxyprovider
+        identifiers:
+          name: Frigate
+        attrs:
+          name: Frigate
+          mode: forward_single
+          external_host: https://frigate.mixeto.io
+          internal_host: ""
+          authorization_flow: !Find [authentik_flows.flow, [slug, default-provider-authorization-implicit-consent]]
+          invalidation_flow: !Find [authentik_flows.flow, [slug, default-provider-invalidation-flow]]
+          internal_host_ssl_validation: true
+          intercept_header_auth: true
+          cookie_domain: ""
+          basic_auth_enabled: false
+          skip_path_regex: ""
+
+      - model: authentik_core.application
+        identifiers:
+          slug: frigate
+        attrs:
+          name: Frigate
+          slug: frigate
+          group: Home
+          meta_icon: https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/frigate.svg
+          meta_launch_url: ""
+          open_in_new_tab: false
+          policy_engine_mode: any
+          provider: !Find [authentik_providers_proxy.proxyprovider, [name, Frigate]]
+  '';
 in
 {
   options.mine.server-home.services.frigate = {
@@ -17,6 +55,8 @@ in
   };
 
   config = mkIf cfg.enable {
+    mine.server-auth.services.authentik.blueprints.frigate = frigateBlueprint;
+
     services.frigate = {
       enable = true;
       hostname = "${hostname}";
