47c9d4e547cf4bba663d045f251b800c4f0d2f7a
Ryan Cole <admin@ryanc.me>
2026-05-09 14:13:55 +1200

    Apply basic Postgres tune
    This is probably way overkill for what I'm doing, but I'll be moving a lot of services from SQLite -> Postgres soon, so maybe it's worthwhile.

--- roles/server-databases/services/postgresql/default.nix
+++ roles/server-databases/services/postgresql/default.nix
@@ -26,6 +26,43 @@ in
     services.postgresql = {
       enable = true;
       package = pkgs."postgresql_${config.mine.server-databases.services.postgresql.version}";
+      settings = {
+        # WARNING
+        # wal_compression = lz4 requires PostgreSQL
+        # to be compiled with --with-lz4
+        #
+        # io_method = io_uring requires PostgreSQL
+        # to be compiled with --with-liburing
+
+        # DB Version: 18
+        # OS Type: linux
+        # DB Type: web
+        # Total Memory (RAM): 8 GB
+        # CPUs num: 8
+        # Connections num: 256
+        # Data Storage: ssd
+
+        max_connections = 256;
+        shared_buffers = "2GB";
+        effective_cache_size = "6GB";
+        maintenance_work_mem = "512MB";
+        checkpoint_completion_target = 0.9;
+        wal_buffers = "16MB";
+        default_statistics_target = 100;
+        random_page_cost = "1.1";
+        effective_io_concurrency = 200;
+        work_mem = "10325kB";
+        huge_pages = "try";
+        jit = "off";
+        wal_compression = "lz4";
+        io_method = "io_uring";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+        max_worker_processes = 8;
+        max_parallel_workers_per_gather = 4;
+        max_parallel_workers = 8;
+        max_parallel_maintenance_workers = 4;
+      };
 
       authentication = ''
         #type   database    DBuser      auth-method   optional_ident_map
