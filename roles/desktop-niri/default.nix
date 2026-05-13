6faa0c6311bf29c7f9bdc36663fb471aff0ec6de
Ryan Cole <admin@ryanc.me>
2026-05-13 12:56:16 +1200

    Add udisks2 + gvfs

--- roles/desktop-niri/default.nix
+++ roles/desktop-niri/default.nix
@@ -6,14 +6,18 @@
 {
   options.mine.desktop-niri.enable = lib.mkEnableOption "'desktop-niri' role";
 
-  config.mine.desktop-niri = lib.mkIf config.mine.desktop-niri.enable {
-    system = {
-      awww.enable = true;
-      niri.enable = true;
-      noctalia.enable = true;
-      power-profiles-daemon.enable = true;
-      sddm.enable = true;
-      upower.enable = true;
+  config = lib.mkIf config.mine.desktop-niri.enable {
+    services.udisks2.enable = true;
+    services.gvfs.enable = true;
+    mine.desktop-niri = {
+      system = {
+        awww.enable = true;
+        niri.enable = true;
+        noctalia.enable = true;
+        power-profiles-daemon.enable = true;
+        sddm.enable = true;
+        upower.enable = true;
+      };
     };
   };
 }
