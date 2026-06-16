5455e58d8b1a6209cd23b4d9e4a851b609fe441b
Ryan Cole <admin@ryanc.me>
2026-06-17 07:58:41 +1200

    Migrate to Noctalia v5

--- home/desktop/niri/default.nix
+++ home/desktop/niri/default.nix
@@ -27,33 +27,9 @@ in
       };
     };
 
-    programs.noctalia-shell = {
+    programs.noctalia = {
       enable = true;
-      plugins = {
-        sources = [
-          {
-            enabled = true;
-            name = "Official Noctalia Plugins";
-            url = "https://github.com/noctalia-dev/noctalia-plugins";
-          }
-        ];
-        states = {
-          catwalk = {
-            enabled = true;
-            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
-          };
-        };
-        version = 1;
-      };
-      # this may also be a string or a path to a JSON file.
-
-      pluginSettings = {
-        catwalk = {
-          minimumThreshold = 25;
-          hideBackground = true;
-        };
-        # this may also be a string or a path to a JSON file.
-      };
+      systemd.enable = true;
     };
 
     # we'll maintain the main config file manually, for now
