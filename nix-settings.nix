e17fc3c0d93a4727df20fa925922fd850814509a
Ryan Cole <admin@ryanc.me>
2026-06-17 08:00:01 +1200

    Add noctalia cachix server

--- nix-settings.nix
+++ nix-settings.nix
@@ -3,9 +3,11 @@
   nix.settings = {
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
     experimental-features = [
       "nix-command"
